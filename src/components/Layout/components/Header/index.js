10e33546defcaae0d96b5f2ad1beceb7ff8a3895
ducthuanlk22222 <92450417+ducthuanlk22222@users.noreply.github.com>
2023-04-06 17:47:34 +0700

    bài 63

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,14 +1,32 @@
 import classNames from 'classnames/bind';
-import styles from '~/components/Layout/components/Header/Header.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const cx = classNames.bind(styles)
+import styles from '~/components/Layout/components/Header/Header.module.scss';
+import images from '~/assets/images';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
 
 function Header() {
-    return <header className={cx('wrapper')}>
-        <div className={cx('inner')}>
-            
-        </div>
-    </header>
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <img src={images.logo} alt="TikTok"></img>
+                <div className={cx('search')}>
+                    <input placeholder="Tìm kiếm tài khoản và video" spellCheck={false} />
+                    {/* <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button> */}
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}></div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
